e80288ac25bb73b0ec5ebce6d8b5039de3f799c6
Tobias Bäthge <git@tobias.baethge.com>
2012-07-28 18:36:47 +0200

    Add minified CSS and JS files

---
+++ admin/quicktags-button.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(b){window.tablepress_open_shortcode_thickbox=function(){var a=b(window).width(),a=720<a?720:a,c=b(window).height();b("body.admin-bar").length&&(c-=28);tb_show(tablepress_editor_button.thickbox_title,tablepress_editor_button.thickbox_url+"&TB_iframe=true&height="+(c-85)+"&width="+(a-80),!1)};"undefined"!=typeof QTags&&QTags.addButton("tablepress_quicktags_button",tablepress_editor_button.caption,tablepress_open_shortcode_thickbox,!1,!1,tablepress_editor_button.title,
+115)});
