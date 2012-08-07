4a56a7e2df5dff0cc0c6ce5c8608f5a4b6b1dde7
Tobias Bäthge <git@tobias.baethge.com>
2012-08-07 17:51:02 +0200

    Add some checks to not load TablePress in areas/on pages where it is never used/necessary

--- classes/class-tablepress.php
+++ classes/class-tablepress.php
@@ -64,6 +64,14 @@ abstract class TablePress {
 	public static function run() {
 		do_action( 'tablepress_run' );
 
+		// exit early if TablePress doesn't have to be loaded
+		if ( ( 'wp-login.php' === basename( $_SERVER['SCRIPT_FILENAME'] ) ) // Login screen
+				|| ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST )
+				|| ( defined( 'APP_REQUEST' ) && APP_REQUEST )
+				|| ( defined( 'DOING_CRON' ) && DOING_CRON ) ) {
+			return;
+		}
+
 		// some filtering of "global" class variables
 		self::$shortcode = apply_filters( 'tablepress_table_shortcode', self::$shortcode );
 
