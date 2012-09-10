57520a3d48fd84a4c5e5ae1ecbb34578fd43fea3
Tobias Bäthge <git@tobias.baethge.com>
2012-09-10 18:24:37 +0200

    Change wording on submit button of "Plugin Options" screen to match that of other WordPress settings/options screens

--- views/view-options.php
+++ views/view-options.php
@@ -50,7 +50,7 @@ class TablePress_Options_View extends TablePress_View {
 		$this->add_meta_box( 'frontend-options', __( 'Frontend Options', 'tablepress' ), array( &$this, 'postbox_frontend_options' ), 'normal' );
 		// $this->add_meta_box( 'backend-options', __( 'Backend Options', 'tablepress' ), array( &$this, 'postbox_backend_options' ), 'normal' ); // @TODO: Commented out as backend options are not yet used
 		$this->add_meta_box( 'user-options', __( 'User Options', 'tablepress' ), array( &$this, 'postbox_user_options' ), 'normal' );
-		$this->data['submit_button_caption'] = __( 'Save Options', 'tablepress' );
+		$this->data['submit_button_caption'] = __( 'Save Changes', 'tablepress' );
 		$this->add_text_box( 'submit', array( &$this, 'textbox_submit_button' ), 'submit' );
 	}
 
