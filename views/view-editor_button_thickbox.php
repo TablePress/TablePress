c35934acf5a4f4173096c0fd5da748b046b623f5
Tobias Bäthge <git@tobias.baethge.com>
2012-11-30 18:58:18 +0100

    Add CSS class to <body> to get button styling back, after WP core's [22948]

--- views/view-editor_button_thickbox.php
+++ views/view-editor_button_thickbox.php
@@ -126,7 +126,7 @@ body {
 }
 </style>
 </head>
-<body class="wp-admin js iframe">
+<body class="wp-admin wp-core-ui js iframe">
 <div id="tablepress-page" class="wrap">
 <?php screen_icon( 'tablepress' ); ?>
 <h2><?php printf( __( '%1$s &lsaquo; %2$s', 'tablepress' ), __( 'List of Tables', 'tablepress' ), 'TablePress' ); ?></h2>
