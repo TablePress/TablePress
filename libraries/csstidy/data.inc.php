3751962be84d8d7384130339967aaeef0d3002eb
TobiasBg <git@tobias.baethge.com>
2013-03-19 22:01:22 +0100

    CSSTidy: Allow color values to be defined multiple times, see http://plugins.trac.wordpress.org/changeset/683731

--- libraries/csstidy/data.inc.php
+++ libraries/csstidy/data.inc.php
@@ -589,7 +589,7 @@ $data['csstidy']['quoted_string_properties'] = array('content', 'font', 'font-fa
  *
  * @global array $data['csstidy']['quoted_string_properties']
  */
-$data['csstidy']['multiple_properties'] = array_merge( $data['csstidy']['unit_values'], array('background', 'background-image', 'transition') );
+$data['csstidy']['multiple_properties'] = array_merge( $data['csstidy']['color_values'], $data['csstidy']['unit_values'], array('background-image', 'transition') );
 
 /**
  * An array containing all predefined templates.
