82fb3f3f2502d6ab4407a2325fd2926e412fddc5
TobiasBg <git@tobias.baethge.com>
2013-08-15 22:27:37 +0200

    PHPUnit bootstrap file: Don't use .. to traverse directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../tablepress.php';
+	require dirname( dirname( __FILE__ ) ) . '/tablepress.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
