38aa85e045339f94b62fef7ae9787e9710f82db2
TobiasBg <git@tobias.baethge.com>
2013-09-01 15:03:21 +0200

    Support formulas in strings, like =Total: {A3+A4}

--- classes/class-render.php
+++ classes/class-render.php
@@ -263,89 +263,110 @@ class TablePress_Render {
 		if ( '' == $content || '=' != $content[0] )
 			return $content;
 
-		$expression = substr( $content, 1 );
+		$content = substr( $content, 1 );
 
-		$replaced_references = $replaced_ranges = array();
+		// Support putting formulas in strings, like =Total: {A3+A4}
+		$expressions = array();
+		if ( preg_match_all( '#{(.+?)}#', $content, $expressions, PREG_SET_ORDER ) ) {
+			$formula_in_string = true;
+		} else {
+			$formula_in_string = false;
+			$expressions[] = array( $content, $content ); // fill array so that it has the same structure as if it came from preg_match_all()
+		}
 
-		// remove all whitespace characters
-		$expression = preg_replace( '#[\r\n\t ]#', '', $expression );
+		foreach ( $expressions as $expression ) {
+			$orig_expression = $expression[0];
+			$expression = $expression[1];
 
-		// expand cell ranges (like A3:A6) to a list of single cells (like A3,A4,A5,A6)
-		if ( preg_match_all( '#([A-Z]+)([0-9]+):([A-Z]+)([0-9]+)#', $expression, $referenced_cell_ranges, PREG_SET_ORDER ) ) {
-			foreach ( $referenced_cell_ranges as $cell_range ) {
-				if ( in_array( $cell_range[0], $replaced_ranges, true ) )
-					continue;
+			$replaced_references = $replaced_ranges = array();
 
-				$replaced_ranges[] = $cell_range[0];
+			// remove all whitespace characters
+			$expression = preg_replace( '#[\r\n\t ]#', '', $expression );
 
-				if ( isset( $this->known_ranges[ $cell_range[0] ] ) ) {
-					$expression = preg_replace( '#(?<![A-Z])' . preg_quote( $cell_range[0], '#' ) . '(?![0-9])#', $this->known_ranges[ $cell_range[0] ], $expression );
-					continue;
-				}
+			// expand cell ranges (like A3:A6) to a list of single cells (like A3,A4,A5,A6)
+			if ( preg_match_all( '#([A-Z]+)([0-9]+):([A-Z]+)([0-9]+)#', $expression, $referenced_cell_ranges, PREG_SET_ORDER ) ) {
+				foreach ( $referenced_cell_ranges as $cell_range ) {
+					if ( in_array( $cell_range[0], $replaced_ranges, true ) )
+						continue;
 
-				// no -1 necessary for this transformation, as we don't actually access the table
-				$first_col = TablePress::letter_to_number( $cell_range[1] );
-				$first_row = $cell_range[2];
-				$last_col = TablePress::letter_to_number( $cell_range[3] );
-				$last_row = $cell_range[4];
+					$replaced_ranges[] = $cell_range[0];
 
-				$col_start = min( $first_col, $last_col );
-				$col_end = max( $first_col, $last_col ) + 1; // +1 for loop below
-				$row_start = min( $first_row, $last_row );
-				$row_end = max( $first_row, $last_row ) + 1; // +1 for loop below
-
-				$cell_list = array();
-				for ( $col = $col_start; $col < $col_end; $col++ ) {
-					for ( $row = $row_start; $row < $row_end; $row++ ) {
-						$column = TablePress::number_to_letter( $col );
-						$cell_list[] = "{$column}{$row}";
+					if ( isset( $this->known_ranges[ $cell_range[0] ] ) ) {
+						$expression = preg_replace( '#(?<![A-Z])' . preg_quote( $cell_range[0], '#' ) . '(?![0-9])#', $this->known_ranges[ $cell_range[0] ], $expression );
+						continue;
 					}
+
+					// no -1 necessary for this transformation, as we don't actually access the table
+					$first_col = TablePress::letter_to_number( $cell_range[1] );
+					$first_row = $cell_range[2];
+					$last_col = TablePress::letter_to_number( $cell_range[3] );
+					$last_row = $cell_range[4];
+
+					$col_start = min( $first_col, $last_col );
+					$col_end = max( $first_col, $last_col ) + 1; // +1 for loop below
+					$row_start = min( $first_row, $last_row );
+					$row_end = max( $first_row, $last_row ) + 1; // +1 for loop below
+
+					$cell_list = array();
+					for ( $col = $col_start; $col < $col_end; $col++ ) {
+						for ( $row = $row_start; $row < $row_end; $row++ ) {
+							$column = TablePress::number_to_letter( $col );
+							$cell_list[] = "{$column}{$row}";
+						}
+					}
+					$cell_list = implode( ',', $cell_list );
+
+					$expression = preg_replace( '#(?<![A-Z])' . preg_quote( $cell_range[0], '#' ) . '(?![0-9])#', $cell_list, $expression );
+					$this->known_ranges[ $cell_range[0] ] = $cell_list;
 				}
-				$cell_list = implode( ',', $cell_list );
-
-				$expression = preg_replace( '#(?<![A-Z])' . preg_quote( $cell_range[0], '#' ) . '(?![0-9])#', $cell_list, $expression );
-				$this->known_ranges[ $cell_range[0] ] = $cell_list;
 			}
+
+			// parse and evaluate single cell references (like A3 or XY312), while prohibiting circle references
+			if ( preg_match_all( '#([A-Z]+)([0-9]+)#', $expression, $referenced_cells, PREG_SET_ORDER ) ) {
+				foreach ( $referenced_cells as $cell_reference ) {
+					if ( in_array( $cell_reference[0], $parents, true ) )
+						return '!ERROR! Circle Reference';
+
+					if ( in_array( $cell_reference[0], $replaced_references, true ) )
+						continue;
+
+					$replaced_references[] = $cell_reference[0];
+
+					$ref_col = TablePress::letter_to_number( $cell_reference[1] ) - 1;
+					$ref_row = $cell_reference[2] - 1;
+
+					if ( ! isset( $this->table['data'][$ref_row] ) || ! isset( $this->table['data'][$ref_row][$ref_col] ) )
+						return "!ERROR! Cell {$cell_reference[0]} does not exist";
+
+					$ref_parents = $parents;
+					$ref_parents[] = $cell_reference[0];
+
+					$result = $this->table['data'][$ref_row][$ref_col] = $this->_evaluate_cell( $this->table['data'][$ref_row][$ref_col], $ref_parents );
+					// Bail if there was an error already
+					if ( false !== strpos( $result, '!ERROR!' ) )
+						return $result;
+					// remove all whitespace characters
+					$result = preg_replace( '#[\r\n\t ]#', '', $result );
+					// Treat empty cells as 0
+					if ( '' == $result )
+						$result = 0;
+					// Bail if the cell does not result in a number (meaning it was a number or expression before being evaluated)
+					if ( ! is_numeric( $result ) )
+						return "!ERROR! {$cell_reference[0]} does not contain a number or expression";
+
+					$expression = preg_replace( '#(?<![A-Z])' . $cell_reference[0] . '(?![0-9])#', $result, $expression );
+				}
+			}
+
+			$result = $this->_evaluate_math_expression( $expression );
+			// Support putting formulas in strings, like =Total: {A3+A4}
+			if ( $formula_in_string )
+				$content = str_replace( $orig_expression, $result, $content );
+			else
+				$content = $result;
 		}
 
-		// parse and evaluate single cell references (like A3 or XY312), while prohibiting circle references
-		if ( preg_match_all( '#([A-Z]+)([0-9]+)#', $expression, $referenced_cells, PREG_SET_ORDER ) ) {
-			foreach ( $referenced_cells as $cell_reference ) {
-				if ( in_array( $cell_reference[0], $parents, true ) )
-					return '!ERROR! Circle Reference';
-
-				if ( in_array( $cell_reference[0], $replaced_references, true ) )
-					continue;
-
-				$replaced_references[] = $cell_reference[0];
-
-				$ref_col = TablePress::letter_to_number( $cell_reference[1] ) - 1;
-				$ref_row = $cell_reference[2] - 1;
-
-				if ( ! isset( $this->table['data'][$ref_row] ) || ! isset( $this->table['data'][$ref_row][$ref_col] ) )
-					return "!ERROR! Cell {$cell_reference[0]} does not exist";
-
-				$ref_parents = $parents;
-				$ref_parents[] = $cell_reference[0];
-
-				$result = $this->table['data'][$ref_row][$ref_col] = $this->_evaluate_cell( $this->table['data'][$ref_row][$ref_col], $ref_parents );
-				// Bail if there was an error already
-				if ( false !== strpos( $result, '!ERROR!' ) )
-					return $result;
-				// remove all whitespace characters
-				$result = preg_replace( '#[\r\n\t ]#', '', $result );
-				// Treat empty cells as 0
-				if ( '' == $result )
-					$result = 0;
-				// Bail if the cell does not result in a number (meaning it was a number or expression before being evaluated)
-				if ( ! is_numeric( $result ) )
-					return "!ERROR! {$cell_reference[0]} does not contain a number or expression";
-
-				$expression = preg_replace( '#(?<![A-Z])' . $cell_reference[0] . '(?![0-9])#', $result, $expression );
-			}
-		}
-
-		return $this->_evaluate_math_expression( $expression );
+		return $content;
 	}
 
 	/**
