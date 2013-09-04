50416cc771d7d360e425bdc7ee293693271d21df
TobiasBg <git@tobias.baethge.com>
2013-09-04 18:47:28 +0200

    Return "unknown" as the username if the display name for a user ID could not be determined

--- classes/class-tablepress.php
+++ classes/class-tablepress.php
@@ -302,7 +302,7 @@ abstract class TablePress {
 	 */
 	public static function get_user_display_name( $user_id ) {
 		$user = get_userdata( $user_id );
-		return ( $user && isset( $user->display_name ) ) ? $user->display_name : '';
+		return ( $user && isset( $user->display_name ) ) ? $user->display_name : __( '<em>unknown</em>', 'tablepress' );
 	}
 
 	/**
