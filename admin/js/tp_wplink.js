34615a8b1ab3f62ea786c3033a655ca3329aa5d4
TobiasBg <git@tobias.baethge.com>
2014-07-25 20:47:26 +0200

    Update wpLink after changeset [29301] in WP Core

--- admin/js/tp_wplink.js
+++ admin/js/tp_wplink.js
@@ -371,6 +371,10 @@ var wpLink;
 				return;
 			}
 
+			if ( inputs.url.is( ':focus' ) || inputs.title.is( ':focus' ) ) {
+				return;
+			}
+
 			fn = event.keyCode === key.UP ? 'prev' : 'next';
 			clearInterval( wpLink.keyInterval );
 			wpLink[ fn ]();
