20d475e62854c9989a43197d1036eb00e3159728
TobiasBg <git@tobias.baethge.com>
2014-11-02 19:38:07 +0100

    Finish TablePress 1.5.

--- classes/class-tablepress.php
+++ classes/class-tablepress.php
@@ -26,7 +26,7 @@ abstract class TablePress {
 	 * @since 1.0.0
 	 * @const string
 	 */
-	const version = '1.4';
+	const version = '1.5';
 
 	/**
 	 * TablePress internal plugin version ("options scheme" version).
@@ -36,7 +36,7 @@ abstract class TablePress {
 	 * @since 1.0.0
 	 * @const int
 	 */
-	const db_version = 26;
+	const db_version = 28;
 
 	/**
 	 * TablePress "table scheme" (data format structure) version.
