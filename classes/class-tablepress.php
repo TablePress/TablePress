5a645a5b956f336c892321616b90854814ce66b2
TobiasBg <git@tobias.baethge.com>
2014-11-07 22:40:21 +0100

    Before checking if the minimum required WordPress version is installed, include an unmodified `$wp_version` to protect from other plugins/themes tempering with it.

--- classes/class-tablepress.php
+++ classes/class-tablepress.php
@@ -114,7 +114,8 @@ abstract class TablePress {
 		}
 
 		// Check if minimum requirements are fulfilled, currently WordPress 4.0.
-		if ( version_compare( str_replace( '-src', '', $GLOBALS['wp_version'] ), '4.0', '<' ) ) {
+		include( ABSPATH . WPINC . '/version.php' ); // Include an unmodified $wp_version.
+		if ( version_compare( str_replace( '-src', '', $wp_version ), '4.0', '<' ) ) {
 			// Show error notice to admins, if WP is not installed in the minimum required version, in which case TablePress will not work.
 			if ( current_user_can( 'update_plugins' ) ) {
 				add_action( 'admin_notices', array( 'TablePress', 'show_minimum_requirements_error_notice' ) );
