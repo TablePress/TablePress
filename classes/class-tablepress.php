abda7e35007c00a19ce0d4504487610b628b84d2
TobiasBg <git@tobias.baethge.com>
2015-04-22 10:56:06 +0200

    Require WordPress 4.2, after changes to how the "Insert Link" button works (and fails in WP 4.1).

--- classes/class-tablepress.php
+++ classes/class-tablepress.php
@@ -113,9 +113,9 @@ abstract class TablePress {
 			return;
 		}
 
-		// Check if minimum requirements are fulfilled, currently WordPress 4.0.
+		// Check if minimum requirements are fulfilled, currently WordPress 4.2.
 		include( ABSPATH . WPINC . '/version.php' ); // Include an unmodified $wp_version.
-		if ( version_compare( str_replace( '-src', '', $wp_version ), '4.0', '<' ) ) {
+		if ( version_compare( str_replace( '-src', '', $wp_version ), '4.2', '<' ) ) {
 			// Show error notice to admins, if WP is not installed in the minimum required version, in which case TablePress will not work.
 			if ( current_user_can( 'update_plugins' ) ) {
 				add_action( 'admin_notices', array( 'TablePress', 'show_minimum_requirements_error_notice' ) );
--- readme.md
+++ readme.md
@@ -41,7 +41,7 @@ For support questions, bug reports, or feature requests, please use the [WordPre
 
 **Requirements?**
 
-In short: WordPress 4.0 or higher, while the latest version of WordPress is always recommended.
+In short: WordPress 4.2 or higher, while the latest version of WordPress is always recommended.
 
 **Languages and Localization?**
 
