5907d3dd880beb81026e5f808dce403184d44809
TobiasBg <git@tobias.baethge.com>
2015-04-25 21:58:36 +0200

    Use same plugin description in tablepress.php and readme.txt.

--- tablepress.php
+++ tablepress.php
@@ -10,7 +10,7 @@
 /*
 Plugin Name: TablePress
 Plugin URI: https://tablepress.org/
-Description: TablePress enables you to create and manage tables in your posts and pages, without having to write HTML code, and it adds valuable functions for your visitors.
+Description: Embed beautiful and feature-rich tables into your posts and pages, without having to write code.
 Version: 1.5.1
 Author: Tobias Bäthge
 Author URI: http://tobias.baethge.com/
