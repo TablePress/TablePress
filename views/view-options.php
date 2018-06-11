14c062d8aad0997043a3af89cb4603c46e90069c
Boone B Gorges <boonebgorges@gmail.com>
2018-06-11 14:41:59 -0500

    view-options: Add label for Custom CSS textarea.

--- views/view-options.php
+++ views/view-options.php
@@ -105,7 +105,7 @@ class TablePress_Options_View extends TablePress_View {
 <table class="tablepress-postbox-table fixed">
 <tbody>
 	<tr>
-		<th class="column-1" scope="row"><?php _e( 'Custom CSS', 'tablepress' ); ?>:</th>
+		<th class="column-1" scope="row"><label for="option-custom-css"><?php _e( 'Custom CSS', 'tablepress' ); ?></label>:</th>
 		<td class="column-2"><label for="option-use-custom-css"><input type="checkbox" id="option-use-custom-css" name="options[use_custom_css]" value="true"<?php checked( $data['frontend_options']['use_custom_css'] ); ?> /> <?php _e( 'Load these &#8220;Custom CSS&#8221; commands to influence the table styling:', 'tablepress' ); ?></label>
 		</td>
 	</tr>
