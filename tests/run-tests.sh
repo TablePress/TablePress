37f203e2124f18f903a1ee0e38ed1e25a0ef1d00
Boone B Gorges <boonebgorges@gmail.com>
2018-12-07 12:46:37 -0600

    Travis CI: Fix phpunit integration. Props and thanks to boonebgorges. Fixes #89 and #105.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,8 +2,8 @@
 
 # Run single-site unit tests:
 export WP_MULTISITE=0
-phpunit --exclude-group=ms-required
+./vendor/bin/phpunit --exclude-group=ms-required
 
 # Run Multisite unit tests:
 export WP_MULTISITE=1
-phpunit --exclude-group=ms-excluded
+./vendor/bin/phpunit --exclude-group=ms-excluded
