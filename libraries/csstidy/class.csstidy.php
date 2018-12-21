138aa66f33cf26e38edb7d251e5ebfcae323d7fd
TobiasBg <git@tobias.baethge.com>
2018-12-21 13:24:26 +0100

    CSSTidy: Fix a PHP7.3 warning. See https://github.com/Cerdic/CSSTidy/commit/6342bdce367bd0b6b66ab715f749e18dae93eaf4

--- libraries/csstidy/class.csstidy.php
+++ libraries/csstidy/class.csstidy.php
@@ -1001,7 +1001,7 @@ class TablePress_CSSTidy {
 						$this->str_char[] = ( '(' === $string[ $i ] ) ? ')' : $string[ $i ];
 						$this->from[] = 'instr';
 						$this->quoted_string[] = ( ')' === $_str_char && '(' !== $string[ $i ] && '(' === trim( $_cur_string ) ) ? $_quoted_string : ( '(' !== $string[ $i ] );
-						continue;
+						continue 2;
 					}
 
 					if ( ')' !== $_str_char && ( "\n" === $string[ $i ] || "\r" === $string[ $i ] ) && ! ( '\\' === $string[ $i - 1 ] && ! $this->escaped( $string, $i - 1 ) ) ) {
