952795af5f508bb9df2b1b2acca9edabfbb21692
TobiasBg <git@tobias.baethge.com>
2019-10-27 22:28:32 +0100

    Import: Detect selection of ZIP files and show appropriate states of select boxes (for choosing an existing table).

--- admin/js/import.js
+++ admin/js/import.js
@@ -14,17 +14,23 @@ jQuery( document ).ready( function( $ ) {
 	'use strict';
 
 	/**
-	 * Show select box for table to replace only if needed
+	 * File extension of the file that is imported.
+	 */
+	var extension = 'csv';
+
+	/**
+	 * Show select box for table to replace only if needed.
 	 *
 	 * @since 1.0.0
 	 */
 	$( '#row-import-type' ).on( 'change', 'input', function() {
-		$( '#tables-import-existing-table' ).prop( 'disabled', ( 'replace' !== $(this).val() && 'append' !== $(this).val() ) );
+		var import_type = $( this ).val();
+		$( '#tables-import-existing-table' ).prop( 'disabled', ( ( 'replace' !== import_type && 'append' !== import_type ) || 'zip' === extension ) );
 	} )
 	.find( 'input:checked' ).change();
 
 	/**
-	 * Show only the import source field that was selected with the radio button
+	 * Show only the import source field that was selected with the radio button.
 	 *
 	 * @since 1.0.0
 	 */
@@ -35,7 +41,7 @@ jQuery( document ).ready( function( $ ) {
 	.find( 'input:checked' ).change();
 
 	/**
-	 * Show only the WP-Table Reloaded import source field that was selected with the radio button
+	 * Show only the WP-Table Reloaded import source field that was selected with the radio button.
 	 *
 	 * @since 1.0.0
 	 */
@@ -46,22 +52,22 @@ jQuery( document ).ready( function( $ ) {
 	.find( 'input:checked' ).change();
 
 	/**
-	 * Select correct value in import format dropdown on file select
+	 * Select correct value in import format dropdown on file select.
 	 *
 	 * @since 1.0.0
 	 */
 	$( '#tables-import-file-upload, #tables-import-url, #tables-import-server' ).on( 'change', function( event ) {
 		var path = $(this).val(),
+			import_type = $( '#row-import-type' ).find( 'input:checked' ).val(),
 			filename_start,
 			extension_start,
-			filename = path,
-			extension = 'csv';
+			filename = path;
 
-		// default extension: CSV for file upload and server, HTML for URL
+		// Default extension: CSV for file upload and server, HTML for URL.
 		if ( 'tables-import-url' === event.target.id ) {
 			extension = 'html';
 		}
-		// determine filename from full path
+		// Determine filename from full path.
 		filename_start = path.lastIndexOf( '\\' );
 		if ( -1 !== filename_start ) { // Windows-based path
 			filename = path.substr( filename_start + 1 );
@@ -71,18 +77,20 @@ jQuery( document ).ready( function( $ ) {
 				filename = path.substr( filename_start + 1 );
 			}
 		}
-		// determine extension from filename
+		// Determine extension from filename.
 		extension_start = filename.lastIndexOf( '.' );
 		if ( -1 !== extension_start ) {
 			extension = filename.substr( extension_start + 1 ).toLowerCase();
 		}
 
-		// allow .htm for HTML as well
+		// Allow .htm for HTML as well.
 		if ( 'htm' === extension ) {
 			extension = 'html';
 		}
 
-		// Don't change the format for ZIP archives
+		$( '#tables-import-existing-table' ).prop( 'disabled', ( ( 'replace' !== import_type && 'append' !== import_type ) || 'zip' === extension ) );
+
+		// Don't change the format for ZIP archives.
 		if ( 'zip' === extension ) {
 			return;
 		}
@@ -101,7 +109,7 @@ jQuery( document ).ready( function( $ ) {
 			valid_form = true,
 			import_type = $( '#row-import-type' ).find( 'input:checked' ).val();
 
-		/* the value of the selected import source field must be set/changed from the default */
+		// Te value of the selected import source field must be set/changed from the default.
 		if ( selected_import_source_field.defaultValue === selected_import_source_field.value ) {
 			$( selected_import_source_field )
 				.addClass( 'invalid' )
@@ -110,8 +118,8 @@ jQuery( document ).ready( function( $ ) {
 			valid_form = false;
 		}
 
-		/* if replace or append is selected, a table must be selected */
-		if ( 'replace' === import_type || 'append' === import_type ) {
+		// If replace or append is selected, a table must be selected - except for ZIP files.
+		if ( ( 'replace' === import_type || 'append' === import_type ) && 'zip' !== extension ) {
 			if ( '' === $( '#tables-import-existing-table' ).val() ) {
 				$( '#row-import-type' )
 					.one( 'change', 'input', function() { $( '#tables-import-existing-table' ).removeClass( 'invalid' ); } );
@@ -123,10 +131,10 @@ jQuery( document ).ready( function( $ ) {
 			}
 		}
 
+		// Don't submit the form if it's not valid.
 		if ( ! valid_form ) {
 			return false;
 		}
-		// at this point, the form is valid and will be submitted
 	} );
 
 	/**
@@ -137,7 +145,7 @@ jQuery( document ).ready( function( $ ) {
 	$( '#tablepress-page' ).find( '#submit_wp_table_reloaded_import' ).on( 'click', function() {
 		$( '#tablepress-page' ).find( 'form' ).off( 'submit.tablepress' );
 
-		/* File upload must have a file, if Dump File is selected as the source */
+		// File upload must have a file, if Dump File is selected as the source.
 		if ( $( '#import-wp-table-reloaded-source-dump-file' ).prop( 'checked' ) && '' === $( '#tables-import-wp-table-reloaded-dump-file' ).val() ) {
 			$( '#tables-import-wp-table-reloaded-dump-file' )
 				.addClass( 'invalid' )
@@ -146,7 +154,7 @@ jQuery( document ).ready( function( $ ) {
 			return false;
 		}
 
-		/* At least one checkbox must be check, to have something imported */
+		// At least one checkbox must be checked, to have something imported.
 		if ( ! $( '#import-wp-table-reloaded-tables' ).prop( 'checked' ) && ! $( '#import-wp-table-reloaded-css' ).prop( 'checked' ) ) {
 			alert( tablepress_import.error_wp_table_reloaded_nothing_selected );
 			return false;
