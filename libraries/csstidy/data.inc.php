559a6d7c05523f95de6a370a565bee9d824b35c2
TobiasBg <git@tobias.baethge.com>
2019-10-28 21:16:05 +0100

    CSSTidy: Support @supports rules. See https://github.com/Cerdic/CSSTidy/commit/5810b0e7cc146c3c9a5075d0b005a836b78d14ae.

--- libraries/csstidy/data.inc.php
+++ libraries/csstidy/data.inc.php
@@ -63,6 +63,7 @@ $data['csstidy']['at_rules'] = array(
 	'import'            => 'iv',
 	'namespace'         => 'iv',
 	'media'             => 'at',
+	'supports'          => 'at',
 	'keyframes'         => 'at',
 	'-moz-keyframes'    => 'at',
 	'-o-keyframes'      => 'at',
