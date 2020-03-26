2ce72acb28f972aeaf30327cde49a2ac08594fb4
TobiasBg <git@tobias.baethge.com>
2020-03-26 14:15:59 +0100

    Excel import: Use correct function name from SimpleXLSX, missed in cea0cb59b7bd570af384d3af4d53aa8275180e2d.

--- classes/class-import.php
+++ classes/class-import.php
@@ -375,7 +375,7 @@ class TablePress_Import {
 		if ( $xlsx_file ) {
 			$this->imported_table = array( 'data' => $xlsx_file->rows() );
 		} else {
-			$output = '<strong>' . __( 'The imported file contains errors:', 'tablepress' ) . '</strong><br /><br />' . SimpleXLSX::parse_error() . '<br />';
+			$output = '<strong>' . __( 'The imported file contains errors:', 'tablepress' ) . '</strong><br /><br />' . SimpleXLSX::parseError() . '<br />';
 			wp_die( $output, 'Import Error', array( 'response' => 200, 'back_link' => true ) );
 		}
 	}
