36b2154c1d80034a21dd194af08f9f6a53d20e13
TobiasBg <git@tobias.baethge.com>
2021-01-01 13:38:35 +0100

    Update TablePress copyright year from 2020 to 2021.

--- tablepress.php
+++ tablepress.php
@@ -20,7 +20,7 @@
  * Donate URI: https://tablepress.org/donate/
  *
  *
- * Copyright 2012-2020 Tobias Bäthge
+ * Copyright 2012-2021 Tobias Bäthge
  *
  * TablePress is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as published by
