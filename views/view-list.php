TablePress/TablePress

commit 1c452469c3abc827442c3047f76f65a3888102a9

Unify handling of dates and dates calculation. Show human diff dates up to one week. Fix 'mysql' format in [table-info] Shortcode to actually show a mySQL date. See https://make.wordpress.org/core/2019/09/23/date-time-improvements-wp-5-3/

views/view-list.php
@@ -504,17 +504,17 @@ class TablePress_All_Tables_List_Table extends WP_List_Table {
 	 * @return string HTML content of the cell.
 	 */
 	protected function column_table_last_modified( array $item ) {
-		$modified_timestamp = strtotime( $item['last_modified'] );
-		$current_timestamp = current_time( 'timestamp' );
+		$modified_timestamp = date_create( $item['last_modified'], wp_timezone() );
+		$modified_timestamp = $modified_timestamp->getTimestamp();
+		$current_timestamp = time();
 		$time_diff = $current_timestamp - $modified_timestamp;
-		// Time difference is only shown up to one day.
-		if ( $time_diff >= 0 && $time_diff < DAY_IN_SECONDS ) {
-			$time_diff = sprintf( __( '%s ago', 'tablepress' ), human_time_diff( $modified_timestamp, $current_timestamp ) );
+		// Time difference is only shown up to one week.
+		if ( $time_diff >= 0 && $time_diff < WEEK_IN_SECONDS ) {
+			$time_diff = sprintf( __( '%s ago', 'default' ), human_time_diff( $modified_timestamp, $current_timestamp ) );
 		} else {
-			$time_diff = TablePress::format_datetime( $item['last_modified'], 'mysql', '<br />' );
+			$time_diff = TablePress::format_datetime( $table['last_modified'], '<br />' );
 		}
-
-		$readable_time = TablePress::format_datetime( $item['last_modified'], 'mysql', ' ' );
+		$readable_time = TablePress::format_datetime( $item['last_modified'] );
 		return '<abbr title="' . esc_attr( $readable_time ) . '">' . $time_diff . '</abbr>';
 	}
 
@@ -684,7 +684,7 @@ class TablePress_All_Tables_List_Table extends WP_List_Table {
 		|| false !== stripos( $item['description'], $term )
 		|| false !== stripos( TablePress::get_user_display_name( $item['author'] ), $term )
 		|| false !== stripos( TablePress::get_user_display_name( $item['options']['last_editor'] ), $term )
-		|| false !== stripos( TablePress::format_datetime( $item['last_modified'], 'mysql', ' ' ), $term )
+		|| false !== stripos( TablePress::format_datetime( $item['last_modified'] ), $term )
 		|| false !== stripos( wp_json_encode( $item['data'], TABLEPRESS_JSON_OPTIONS ), $json_encoded_term ) ) {
 			return true;
 		}

classes/class-controller.php
@@ -99,7 +99,7 @@ abstract class TablePress_Controller {
 			if ( 0 === TablePress::$model_options->get( 'first_activation' ) ) {
 				// Save initial set of plugin options, and time of first activation of the plugin, on first activation.
 				TablePress::$model_options->update( array(
-					'first_activation'          => current_time( 'timestamp' ),
+					'first_activation'          => time(),
 					'plugin_options_db_version' => TablePress::db_version,
 				) );
 			} else {

classes/class-tablepress.php
@@ -342,18 +342,16 @@ abstract class TablePress {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param string|int $datetime  DateTime string in mySQL format or a Unix timestamp integer.
-	 * @param string     $type      Optional. Type of $datetime, 'mysql' or 'timestamp'.
-	 * @param string     $separator Optional. Separator between date and time.
+	 * @param string $datetime  DateTime string, often in mySQL format..
+	 * @param string $separator Optional. Separator between date and time.
 	 * @return string Nice looking string with the date and time.
 	 */
-	public static function format_datetime( $datetime, $type = 'mysql', $separator = ' ' ) {
-		// @TODO: Maybe change from using the stored WP Options to translated date/time schemes, like in https://core.trac.wordpress.org/changeset/35811.
-		if ( 'mysql' === $type ) {
-			return mysql2date( get_option( 'date_format' ), $datetime ) . $separator . mysql2date( get_option( 'time_format' ), $datetime );
-		} else {
-			return date_i18n( get_option( 'date_format' ), $datetime ) . $separator . date_i18n( get_option( 'time_format' ), $datetime );
-		}
+	public static function format_datetime( $datetime, $separator = ' ' ) {
+		$timezone = wp_timezone();
+		$datetime = date_create( $datetime, $timezone );
+		$date = wp_date( get_option( 'date_format' ), $datetime->getTimestamp(), $timezone );
+		$time = wp_date( get_option( 'time_format' ), $datetime->getTimestamp(), $timezone );
+		return "{$date}{$separator}{$time}";
 	}
 
 	/**

controllers/controller-admin.php
@@ -943,7 +943,7 @@ class TablePress_Admin_Controller extends TablePress_Controller {
 			@set_time_limit( 300 );
 
 			$zip_file = new ZipArchive();
-			$download_filename = sprintf( 'tablepress-export-%1$s-%2$s.zip', date_i18n( 'Y-m-d-H-i-s' ), $export['format'] );
+			$download_filename = sprintf( 'tablepress-export-%1$s-%2$s.zip', wp_date( 'Y-m-d-H-i-s' ), $export['format'] );
 			$download_filename = sanitize_file_name( $download_filename );
 			$full_filename = wp_tempnam( $download_filename );
 			if ( true !== $zip_file->open( $full_filename, ZIPARCHIVE::OVERWRITE ) ) {

controllers/controller-frontend.php
@@ -742,30 +742,33 @@ JS;
 			case 'last_modified':
 				switch ( $format ) {
 					case 'raw':
+					case 'mysql':
 						$output = $table['last_modified'];
 						break;
 					case 'human':
-						$modified_timestamp = strtotime( $table['last_modified'] );
-						$current_timestamp = current_time( 'timestamp' );
+						$modified_timestamp = date_create( $table['last_modified'], wp_timezone() );
+						$modified_timestamp = $modified_timestamp->getTimestamp();
+						$current_timestamp = time();
 						$time_diff = $current_timestamp - $modified_timestamp;
-						// Time difference is only shown up to one day.
-						if ( $time_diff >= 0 && $time_diff < DAY_IN_SECONDS ) {
+						// Time difference is only shown up to one week.
+						if ( $time_diff >= 0 && $time_diff < WEEK_IN_SECONDS ) {
 							$output = sprintf( __( '%s ago', 'default' ), human_time_diff( $modified_timestamp, $current_timestamp ) );
 						} else {
-							$output = TablePress::format_datetime( $table['last_modified'], 'mysql', '<br />' );
+							$output = TablePress::format_datetime( $table['last_modified'], '<br />' );
 						}
 						break;
 					case 'date':
-						$modified_timestamp = strtotime( $table['last_modified'] );
-						$output = date_i18n( get_option( 'date_format' ), $modified_timestamp );
+						$timezone = wp_timezone();
+ 						$datetime = date_create( $table['last_modified'], $timezone );
+ 						$output = wp_date( get_option( 'date_format' ), $datetime->getTimestamp(), $timezone );
 						break;
 					case 'time':
-						$modified_timestamp = strtotime( $table['last_modified'] );
-						$output = date_i18n( get_option( 'time_format' ), $modified_timestamp );
+						$timezone = wp_timezone();
+ 						$datetime = date_create( $table['last_modified'], $timezone );
+ 						$output = wp_date( get_option( 'time_format' ), $datetime->getTimestamp(), $timezone );
 						break;
-					case 'mysql':
 					default:
-						$output = TablePress::format_datetime( $table['last_modified'], 'mysql', ' ' );
+						$output = TablePress::format_datetime( $table['last_modified'] );
 						break;
 				}
 				break;

models/model-table.php
@@ -872,8 +872,8 @@ class TablePress_Table_Model extends TablePress_Model {
 			'name'          => '',
 			'description'   => '',
 			'data'          => array( array( '' ) ), // one empty cell
-			// 'created' => current_time( 'mysql' ),
-			'last_modified' => current_time( 'mysql' ),
+			// 'created' => wp_date( 'Y-m-d H:i:s' ),
+			'last_modified' => wp_date( 'Y-m-d H:i:s' ),
 			'author'        => get_current_user_id(),
 			'options'       => array(
 				'last_editor'                 => get_current_user_id(),
@@ -980,8 +980,8 @@ class TablePress_Table_Model extends TablePress_Model {
 		$table['description'] = $new_table['description'];
 		$table['data'] = $new_table['data'];
 		// $table['author'] = get_current_user_id(); // We don't want this, as it would override the original author.
-		// $table['created'] = current_time( 'mysql' ); // We don't want this, as it would override the original datetime.
-		$table['last_modified'] = current_time( 'mysql' );
+		// $table['created'] = wp_date( 'Y-m-d H:i:s' ); // We don't want this, as it would override the original datetime.
+		$table['last_modified'] = wp_date( 'Y-m-d H:i:s' );
 		$table['options']['last_editor'] = get_current_user_id();
 		// Table Options.
 		if ( isset( $new_table['options'] ) ) { // is for example not set for newly added tables

views/view-editor_button_thickbox.php
@@ -417,7 +417,7 @@ class TablePress_Editor_Button_Thickbox_List_Table extends WP_List_Table {
 		|| false !== stripos( $item['name'], $term )
 		|| false !== stripos( $item['description'], $term )
 		|| false !== stripos( TablePress::get_user_display_name( $item['author'] ), $term )
-		|| false !== stripos( TablePress::format_datetime( $item['last_modified'], 'mysql', ' ' ), $term )
+		|| false !== stripos( TablePress::format_datetime( $item['last_modified'] ), $term )
 		|| false !== stripos( wp_json_encode( $item['data'], TABLEPRESS_JSON_OPTIONS ), $json_encoded_term ) ) {
 			return true;
 		}

commit bf84d191cbbc08886825021d6ea07e1620ad23eb

"List all tables" screen: Fix wrong variable name. Broken in 1c452469c3abc827442c3047f76f65a3888102a9.

views/view-list.php
@@ -513,7 +513,7 @@ class TablePress_All_Tables_List_Table extends WP_List_Table {
 		if ( $time_diff >= 0 && $time_diff < WEEK_IN_SECONDS ) {
 			$time_diff = sprintf( __( '%s ago', 'default' ), human_time_diff( $modified_timestamp, $current_timestamp ) );
 		} else {
-			$time_diff = TablePress::format_datetime( $table['last_modified'], '<br />' );
+			$time_diff = TablePress::format_datetime( $item['last_modified'], '<br />' );
 		}
 		$readable_time = TablePress::format_datetime( $item['last_modified'] );
 		return '<abbr title="' . esc_attr( $readable_time ) . '">' . $time_diff . '</abbr>';